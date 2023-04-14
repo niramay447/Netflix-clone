f9564607937313deb8b8ebb13f80f073c6aeb516
niramay447 <nk447@snu.edu.in>
2023-04-14 19:26:27 +0530

    gradient fade for the heroImage. TODO: fix for dark mode and light mode.

--- Netflix-clone/Views/HeroHeaderUIView.swift
+++ Netflix-clone/Views/HeroHeaderUIView.swift
@@ -18,12 +18,25 @@ class HeroHeaderUIView: UIView {
         imageView.image = UIImage(named: "heroImage")
         return imageView
     }()
+    
+//    custom function for the gradient effect
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(heroImageView)
-        
+        addGradient()
     }
+    
+    
     
     override func layoutSubviews() {
         super.layoutSubviews()
